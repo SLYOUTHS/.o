2d97285f7b4d00ead036fac1281195a31d7d7360
SLYOUTHS <joshuadanielpad@gmail.com>
2025-07-23 17:21:48 -0400

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,3 +1,11 @@
-if (/Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    window.location.href = "index-phone.html";
-}
+function isMobilePhone() {
+    const ua = navigator.userAgent;
+    // Matches Android phones, iPhones, and most mobile phones, but excludes iPad and Tablet
+    const isMobile = /Mobi|Android|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua);
+    const isTablet = /iPad|Tablet/i.test(ua);
+    return isMobile && !isTablet;
+  }
+
+  if (isMobilePhone()) {
+    window.location.href = "mobile.html"; // Change this to your target HTML file
+  }
